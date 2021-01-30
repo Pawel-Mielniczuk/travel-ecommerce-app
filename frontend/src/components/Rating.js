31bf6e9a928eb7febfdbe0c23ebc1cb787b52ffb
Pawel Mielniczuk <mrjeronimo@onet.pl>
2021-01-30 20:21:55 +0100

    create Rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const Rating = ({ value, text, color }) => (
+  <div className='rating'>
+    <span style={{color}}>
+      <span 
+        className={value >=1 
+          ? 'fas fa-star'
+          : value >= 0.5 
+          ? 'fas fa-star-half-alt'
+          : 'fas fa-star'}
+          >
+      </span>
+    </span>
+    <span style={{color}}>
+      <span 
+        className={value >=3
+          ? 'fas fa-star'
+          : value >= 2.5 
+          ? 'fas fa-star-half-alt'
+          : 'fas fa-star'}
+          >
+      </span>
+    </span>
+    <span style={{color}}>
+      <span 
+        className={value >=4 
+          ? 'fas fa-star'
+          : value >= 3.5 
+          ? 'fas fa-star-half-alt'
+          : 'fas fa-star'}
+          >
+      </span>
+    </span>
+    <span style={{color}}>
+      <span 
+        className={value >=5
+          ? 'fas fa-star'
+          : value >= 4.5 
+          ? 'fas fa-star-half-alt'
+          : 'fas fa-star'}
+          >
+      </span>
+    </span>
+    <span style={{color}}>
+      <span 
+        className={value >=5
+          ? 'fas fa-star'
+          : value >= 4.5 
+          ? 'fas fa-star-half-alt'
+          : 'fas fa-star'}
+          >
+      </span>
+    </span>
+    <span>{text ? text : ''}</span>
+  </div>
+)
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+
+export default Rating;
