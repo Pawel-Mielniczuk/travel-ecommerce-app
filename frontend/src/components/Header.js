bb3321207c5c2ecff2f3f7afc433040eaacad42a
Pawel Mielniczuk <mrjeronimo@onet.pl>
2021-01-30 21:08:59 +0100

    Implement router to login and cart in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,34 @@
+//React
 import React from 'react'
+//Bootstrap
 import { Navbar, Nav, Container }  from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" expand="lg" variant='dark' collapseOnSelect>
       <Container>
-  <Navbar.Brand href="#/">Travel shop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="#cart"><span className='fas fa-shopping-cart'></span> Cart</Nav.Link>
-      <Nav.Link href="#login"><span className='fas fa-user'></span> Sign-in</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <LinkContainer to='/'>
+        <Navbar.Brand href="#/">Travel shop</Navbar.Brand>
+      </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+        <LinkContainer to='/cart'>
+          <Nav.Link >
+            <span className='fas fa-shopping-cart'></span> Cart
+          </Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='login'>
+          <Nav.Link>
+            <span className='fas fa-user'></span> Sign-in
+          </Nav.Link>
+        </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+      </Navbar>
     </header>
   )
 };
