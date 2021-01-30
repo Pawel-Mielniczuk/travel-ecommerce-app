26a9a1bf4bf2a26276a12264a52a90e5291815b2
Pawel Mielniczuk <mrjeronimo@onet.pl>
2021-01-30 19:46:24 +0100

    Add images and dummy data as product.js

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Salewa Mtn trainer gtx',
+    image: '/images/buty_salewa_mtn.png',
+    description:
+      'Built like a trail-running shoe but with added ankle support and protection, the Salewa Mtn gtx is our favorite all-around hiking boot for 2021',
+    brand: 'Salewa',
+    category: 'Hiking boots',
+    price: 109.99,
+    countInStock: 10,
+    rating: 4.7,
+    numReviews: 11,
+  },
+  {
+    _id: '2',
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/buty_trekingowe_mtn.png',
+    description:
+      'For day hikers and lightweight backpackers who stick mostly to maintained trails, our top value pick is the Salewa mtn. What makes this boot so popular is its lightweight and comfortable feel at such a reasonable price. ',
+    brand: 'Salewa',
+    category: 'Hiking boots',
+    price: 79.99,
+    countInStock: 7,
+    rating: 4.2,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Skarpa hike gtx',
+    image: '/images/buty_trekingowe_skarpa.png',
+    description:
+      'If you’re in the market for a tough boot for serious day hiking and backpacking, Skarpa GTX is the whole package. The third generation of the line features an aggressive outsole that grips well in just about all conditions, along with a moderately flexible platform for improved comfort',
+    brand: 'Skarpa',
+    category: 'Hiking boots',
+    price: 229.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Zamberlan Gtx mid',
+    image: '/images/buty_trekingowe_zamberlan.png',
+    description:
+      'The Lowa Renegade has the look and feel of a traditional hiking boot at an impressively low weight. Unlike the nimble and more modern Quest above, the leather Renegade offers better isolation from the ground and feels more planted and sturdy',
+    brand: 'Sony',
+    category: 'Hiking boots',
+    price: 199.99,
+    countInStock: 11,
+    rating: 4.4,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Smartwool hike crew',
+    image: '/images/skarpety_hike_medium.jpg',
+    description:
+      'Smart wool Hiker Micro Crew Cushion  socks provide miles of comfort and durability—tested on the Vermont Long Trail, these socks provide a high level of cushioned performance.',
+    brand: 'Smartwool',
+    category: 'socks',
+    price: 49.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/skarpety_smartwool_hike.jpg',
+    description:
+      ' Hiker Quarter Cushion socks deliver what you need on the trail. A Performance Trail fit means no bunching, no slipping, and no blisters. Side vents deliver superior breathability.',
+    brand: 'Smartwool',
+    category: 'socks',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
