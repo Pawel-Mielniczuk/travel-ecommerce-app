baae0b32e46d94444e4e3d30e5d368a9957829a2
Pawel Mielniczuk <mrjeronimo@onet.pl>
2021-01-30 21:11:52 +0100

    update App and Product components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import ProductDetails from './components/ProductDetails';
 
 const App = () => (
- 
-    <div className="App">
-     Travel shop
-    </div>
+    <Router>
+        <Header/>
+        <main className='py-3'>
+        <Container>
+          <Route path='/' component={Home} exact />
+          <Route path='/product/:id' component={ProductDetails} />
+        </Container>
+        </main>
+      <Footer/>
+    </Router>
 )
 
 export default App;
